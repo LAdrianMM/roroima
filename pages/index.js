87300ce2abf3a5b665546b81a87177bd2034d391
Luis Matos <ladrianmm@gmail.com>
2022-03-22 16:03:42 -0300

    Copy changes

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,6 @@ export default function Home() {
 
             <main className={styles.main}>
                 <div className={styles.grid}>
-                    <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-                        <h2>Examples &rarr;</h2>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
                     <a
                         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                         className={styles.card}
